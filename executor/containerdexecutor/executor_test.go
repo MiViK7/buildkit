7728897601c86b96cc7227aaaa5dc639904e7d19
Pat <188729543+MiViK7@users.noreply.github.com>
2026-04-11 15:12:22 +0200

    Delete executor directory
    Signed-off-by: Pat <188729543+MiViK7@users.noreply.github.com>

--- executor/containerdexecutor/executor_test.go
+++
@@ -1,2 +0,0 @@
-wayapi "github.com/moby/buildkit/frontend/g There are assumptions in the containerd executor that the UnknownExitStatus
-	// used in errdefs.ExitError matches the variable in the containnknownExitStatus != gatewayapi.Un
