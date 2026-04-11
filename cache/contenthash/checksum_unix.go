1774cb815e29b8dff5b0c195f786e50478e409f5
Pat <188729543+MiViK7@users.noreply.github.com>
2026-04-11 15:11:20 +0200

    Delete cache directory
    Signed-off-by: Pat <188729543+MiViK7@users.noreply.github.com>

--- cache/contenthash/checksum_unix.go
+++
@@ -1,9 +0,0 @@
-//go:build !windows
-
-package contenthash
-
-import "path/filepath"
-
-func (cc *cacheContext) walk(scanPath string, walkFunc filepath.WalkFunc) error {
-	return filepath.Walk(scanPath, walkFunc)
-}
